81e9319c003519464bd6cd45db06d4d5e7c5fa53
Chris Williams <chris.williams@dvla.gov.uk>
2020-02-02 22:23:55 +0000

    tech blog

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteMetadata: {
+    title: `Technical Blog`,
+    description: `This is my coding blog where I write about my cloud journey.`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`,
+      },
+    },
+  ],
+};
